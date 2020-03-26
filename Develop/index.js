8f669fd6083b3fad49f4b9ff837d962bf3002261
RAW <rwilliams01101@gmail.com>
2020-03-26 15:27:15 -0500

    successful API call to github

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,8 @@
+const axios =require('axios');
+const util =require('util');
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 const questions = [
 
 ];
@@ -6,7 +11,20 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
-
+    axios
+    .get("https://api.github.com/users/rwilliams01101")
+    .then(function(res) {
+        console.log(res.data);
+    })
 }
 
 init();
+
+// take user input for user name
+    //
+// get github info
+    // project title, description, tOc, instal, useage, license, contributing, tests, questions
+    // user github profile picture, user github email
+
+// create new readme.md file
+// populate readme with info from github
