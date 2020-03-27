1d3720459b0f82760ef59d4c875eded51935a612
RAW <rwilliams01101@gmail.com>
2020-03-26 23:41:56 -0500

    updated code for append

--- Develop/index.js
+++ Develop/index.js
@@ -79,29 +79,29 @@ async function init() {
         var avatar = res.data.avatar_url;
         var email = "rwilliams01101@gmail.com";
 
+        writeFileAsync("README.md", `![image of ${username}](${avatar})` + "\n", function () {});
+        appendFileAsync("README.md", "* " + `Email: [${email}](${email})` + "\n", function() {});
+        appendFileAsync("README.md", `## Title` + "\n")
+        appendFileAsync("README.md", "### " + title  + "\n", function() {});
+        appendFileAsync("README.md", `## Description` + "\n")
+        appendFileAsync("README.md", description  + "\n", function() {});
+        appendFileAsync("README.md", `## Table of Contents` + "\n")
+        appendFileAsync("README.md", "* " + tableOfContents  + "\n", function() {});
+        appendFileAsync("README.md", `## Installation` + "\n")
+        appendFileAsync("README.md", installation  + "\n", function() {});
+        appendFileAsync("README.md", `## Usage` + "\n")
+        appendFileAsync("README.md", "* " + usage  + "\n", function() {});
+        appendFileAsync("README.md", `## License` + "\n")
+        appendFileAsync("README.md", `![shields.io](https://camo.githubusercontent.com/5b17d82d9a87c80cdd019bacb35c23f3515d33c3/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c6963656e73652d417061636865253230322e302d79656c6c6f77677265656e2e737667)`, function() {});
+        appendFileAsync("README.md", "* " + license  + "\n", function() {});
+        appendFileAsync("README.md", `## Contributing` + "\n")
+        appendFileAsync("README.md", "* " + contributing  + "\n", function() {});
+        appendFileAsync("README.md", `## Tests` + "\n")
+        appendFileAsync("README.md", "* " + tests  + "\n", function() {});
+        appendFileAsync("README.md", `## Questions` + "\n")
+        appendFileAsync("README.md", "* " + questions  + "\n", function() {});
       });
       
-      writeFileAsync("README.md", `![image of ${username}](${avatar})` + "\n", function () {});
-      appendFileAsync("README.md", "* " + `Email: [${email}](${email})` + "\n", function() {});
-      appendFileAsync("README.md", `## Title` + "\n")
-      appendFileAsync("README.md", "### " + title  + "\n", function() {});
-      appendFileAsync("README.md", `## Description` + "\n")
-      appendFileAsync("README.md", description  + "\n", function() {});
-      appendFileAsync("README.md", `## Table of Contents` + "\n")
-      appendFileAsync("README.md", "* " + tableOfContents  + "\n", function() {});
-      appendFileAsync("README.md", `## Installation` + "\n")
-      appendFileAsync("README.md", installation  + "\n", function() {});
-      appendFileAsync("README.md", `## Usage` + "\n")
-      appendFileAsync("README.md", "* " + usage  + "\n", function() {});
-      appendFileAsync("README.md", `## License` + "\n")
-      appendFileAsync("README.md", `![shields.io](https://camo.githubusercontent.com/5b17d82d9a87c80cdd019bacb35c23f3515d33c3/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c6963656e73652d417061636865253230322e302d79656c6c6f77677265656e2e737667)`, function() {});
-      appendFileAsync("README.md", "* " + license  + "\n", function() {});
-      appendFileAsync("README.md", `## Contributing` + "\n")
-      appendFileAsync("README.md", "* " + contributing  + "\n", function() {});
-      appendFileAsync("README.md", `## Tests` + "\n")
-      appendFileAsync("README.md", "* " + tests  + "\n", function() {});
-      appendFileAsync("README.md", `## Questions` + "\n")
-      appendFileAsync("README.md", "* " + questions  + "\n", function() {});
     });
 }
 
